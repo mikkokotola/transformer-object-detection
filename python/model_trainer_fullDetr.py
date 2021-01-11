b26db88c3fa98f3408a3761aeb4c6045fb0afc14
Mikko Kotola <mikko.kotola@sympa.com>
2021-01-11 22:10:09 +0200

    Add params

--- python/model_trainer_fullDetr.py
+++ python/model_trainer_fullDetr.py
@@ -5,7 +5,8 @@ from loss import loss, getBipartiteMatching
 from train import train
 
 device = torch.device("cuda")
-model = DETR().to(device)
+model = DETR(num_classes=91, hidden_dim=100, nheads=10,
+                 num_encoder_layers=2, num_decoder_layers=2).to(device)
 
 trainDataset = CocoDataset(100, "../data/annotations/cleaned_train_image_data.csv", "../data/annotations/cleaned_train_labels_data.csv", "../data/train2017")
 valDataset = CocoDataset(100, "../data/annotations/cleaned_val_image_data.csv", "../data/annotations/cleaned_val_labels_data.csv", "../data/val2017")
